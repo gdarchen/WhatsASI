92ef9b332951a790e3365847cc7d6d28e1467a19
Alexandre Huat <alexandre.huat@insa-rouen.fr>
2017-04-05 12:52:24 +0200

    ajout d'un mot filtre depuis un textfield

--- WhatsASI/src/whatsasi/client/MessagerieClient.java
+++ WhatsASI/src/whatsasi/client/MessagerieClient.java
@@ -9,31 +9,36 @@ import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
-import javafx.scene.Group;
-import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
-import java.util.Observable;
+import java.util.logging.Filter;
 
 public class MessagerieClient extends Application {
+    //==== Menu nodes
     TitledPane connexionPane = new TitledPane();
     TitledPane chatPane = new TitledPane();
     TitledPane filterPane = new TitledPane();
     Accordion accordion = new Accordion(connexionPane, filterPane, chatPane);
     Rectangle2D screenSize = Screen.getPrimary().getBounds();
 
+    //==== Connexion nodes
     ImageView avatar = new ImageView(new Image("https://i1.social.s-msft.com/profile/u/avatar.jpg?displayname=kabir+shenvi&size=extralarge&version=00000000-0000-0000-0000-000000000000", 120, 120, true, false));
     TextField pseudoTextField = new TextField();
     Button connexionOK = new Button("Se connecter");
+
+    //==== Filter nodes
+    TextField filterTextField = new TextField();
+    Button addFilteredWord = new Button("Ajouter");
+    Label filterTextFieldAlert = new Label("Ce mot est déjà dans la liste !");
+    ObservableList<String> filterList;
     Button filterOK = new Button("Valider");
 
     private void initConnexionPane() {
@@ -56,7 +61,6 @@ public class MessagerieClient extends Application {
         vbox.getChildren().add(connexionOK);
         connexionOK.setOnAction(new ConnexionEventHandler());
 
-
         connexionPane.setText("Connexion");
         connexionPane.setContent(vbox);
     }
@@ -71,11 +75,31 @@ public class MessagerieClient extends Application {
     private void initFilterPane() {
         VBox vbox = new VBox(12);
 
-        ObservableList<FilterCell> list = FXCollections.observableArrayList(new FilterCell("putain"),
-                new FilterCell("Voldemort"));
-        FilterListView filterListView = new FilterListView();
+        filterList = FXCollections.observableArrayList("putain");/*new FilterCell("putain"),
+                new FilterCell("Voldemort"),
+                new FilterCell("Donald Trump"));*/
+        FilterListView filterListView = new FilterListView(filterList);
+
+        vbox.getChildren().add(new Label("Saisissez les mots à filtrer. Les mots filtrés seront remplacés " +
+                                         "par des étoiles dans les conversations. Par exemple, \"gros mot\" sera " +
+                                         "remplacé par \"**** ***\". Le filtrage est indépendant de la casse"));
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(6, 6, 6, 6));
+        grid.setVgap(6);
+        grid.setHgap(6);
+        grid.add(new Label("Saisissez un mot-filtre :"), 0, 0);
+        grid.add(filterTextField, 1, 0);
+        grid.add(addFilteredWord, 2, 0);
+        filterTextFieldAlert.setStyle("-fx-text-fill: red");
+        filterTextFieldAlert.setVisible(false);
+        grid.add(filterTextFieldAlert, 1, 1);
+        grid.setAlignment(Pos.BASELINE_LEFT);
+        addFilteredWord.setOnAction(new AddFilteredWordEventHandler());
+        vbox.getChildren().add(grid);
 
         vbox.getChildren().add(filterListView);
+
+        filterOK.setAlignment(Pos.BASELINE_RIGHT);
         vbox.getChildren().add(filterOK);
 
         filterPane.setText("Filtres");
@@ -83,34 +107,67 @@ public class MessagerieClient extends Application {
         filterPane.setContent(vbox);
     }
 
-    public class FilterListView extends ListView<FilterCell> {
+    public class AddFilteredWordEventHandler implements EventHandler<ActionEvent> {
+        @Override
+        public void handle(ActionEvent e) {
+            String text = filterTextField.getText().trim().toLowerCase();
+            if (!filterList.contains(text)) {
+                System.out.println("Mot-filtre ajouté : " + text);
+                filterList.add(text);
+                filterTextFieldAlert.setVisible(false);
+            } else {
+                filterTextFieldAlert.setVisible(true);
+            }
+        }
+    }
+
+    public class FilterListView extends ListView<String> {
         public FilterListView() {
             super();
+            initCellFactory();
         }
 
-        public FilterListView(ObservableList<FilterCell> items) {
+        public FilterListView(ObservableList<String> items) {
             super(items);
+            initCellFactory();
+        }
+
+        public void initCellFactory() {
+            setCellFactory(new Callback<ListView<String>, ListCell<String>>() {
+                @Override
+                public ListCell<String> call(ListView<String> param) {
+                    return new FilterCell();
+                }
+            });
         }
     }
 
     public class FilterCell extends ListCell<String> {
-        private TextField text = new TextField("");
+        private TextField textField = new TextField("");
         private Button plus = new Button("+");
         private Button minus = new Button("-");
 
+        public FilterCell() {
+            setTextField(null);
+            initCell();
+        }
+
         public FilterCell(String text) {
             setTextField(text);
-            initLayout();
+            initCell();
         }
 
         public void setTextField(String text) {
-            setText(text);
-            this.text.setText(text);
+            if (text != null) {
+                text = text.trim().toLowerCase();
+            }
+            setText(null);
+            textField.setText(text);
         }
 
-        public void initLayout() {
+        public void initCell() {
             GridPane gridPane = new GridPane();
-            gridPane.add(text, 0, 0);
+            gridPane.add(textField, 0, 0);
             gridPane.add(plus, 1, 0);
             gridPane.add(minus, 2, 0);
             setGraphic(gridPane);
@@ -120,12 +177,17 @@ public class MessagerieClient extends Application {
         public void updateItem(String text, boolean empty) {
             super.updateItem(text, empty);
             if (empty) {
-                setTextField(null);
+                // setTextField(null);
                 setGraphic(null);
             } else {
                 setTextField(text);
-                initLayout();
+                initCell();
             }
+        }
+
+        @Override
+        public String toString() {
+            return textField.getText();
         }
     }
 
