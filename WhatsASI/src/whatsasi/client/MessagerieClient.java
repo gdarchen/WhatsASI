29f8577d6c87a9309902a3520a106403588b5d09
Gautier DARCHEN <gautier.darchen@insa-rouen.fr>
2017-04-18 18:29:54 +0200

    Ajout du TextField de nouveau message, du Button d'envoi, du Button pour ajouter des conversations, et gestion de tous les layouts imbriqués (VBox, HBox, GridPane...).

--- WhatsASI/src/whatsasi/client/MessagerieClient.java
+++ WhatsASI/src/whatsasi/client/MessagerieClient.java
@@ -12,6 +12,7 @@ import javafx.geometry.Rectangle2D;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
@@ -43,9 +44,10 @@ public class MessagerieClient extends Application {
 
     //==== Chat nodes
     ObservableList<String> items;
-    Label labelConv = new Label("Conversations");
-
-
+    ObservableList<String> convList;
+    Button addConvButton;
+    TextField nouveauMessage;
+    Button sendMessage;
 
     private void initConnexionPane() {
         VBox vbox = new VBox(12);
@@ -218,19 +220,119 @@ public class MessagerieClient extends Application {
         listeConv.setItems(items);
         listeConv.setPrefHeight(0.7*screenSize.getHeight());
 
-        ListView<String> listeMessages = new ListView<String>();
-        ObservableList<String> messages =FXCollections.observableArrayList (
-            "Single", "Double", "Suite", "Family App");
-        listeMessages.setItems(messages);
-        listeMessages.setPrefHeight(0.7*screenSize.getHeight());
-        listeMessages.setPrefWidth(0.75*screenSize.getWidth());
+        addConvButton = new Button("+");
 
-        grid.add(labelConv,1, 1);
-        grid.add(listeConv, 1, 2);
-        grid.add(listeMessages,3,2);
+        VBox vboxConv = new VBox();
+        vboxConv.setPadding(new Insets(10));
+        vboxConv.setSpacing(8);
+
+        vboxConv.getChildren().add(listeConv);
+        vboxConv.getChildren().add(addConvButton);
+
+
+        VBox vbox = new VBox();
+        vbox.setPadding(new Insets(10));
+        vbox.setSpacing(8);
+
+        convList = FXCollections.observableArrayList("C'est qui ?", "C'est Pinpin !");
+        ConvListView convListView = new ConvListView(convList);
+
+        convListView.setPrefHeight(0.7*screenSize.getHeight());
+        convListView.setPrefWidth(0.75*screenSize.getWidth());
+
+
+        nouveauMessage = new TextField();
+        nouveauMessage.setPromptText("Nouveau message");
+        sendMessage = new Button("Envoyer");
+        nouveauMessage.setPrefWidth(0.65*screenSize.getWidth());
+        sendMessage.setPrefWidth(0.10*screenSize.getWidth());
+        HBox hbox = new HBox();
+        hbox.setPadding(new Insets(15, 12, 15, 12));
+        hbox.setSpacing(10);
+        hbox.getChildren().add(nouveauMessage);
+        hbox.getChildren().add(sendMessage);
+
+
+        vbox.getChildren().add(convListView);
+        vbox.getChildren().add(hbox);
+
+        grid.add(vboxConv, 1, 1);
+        grid.add(vbox, 3, 1);
         chatPane.setContent(grid);
     }
 
+
+
+    public class ConvListView extends ListView<String> {
+        public ConvListView() {
+            super();
+            initMessageCellFactory();
+        }
+
+        public ConvListView(ObservableList<String> items) {
+            super(items);
+            initMessageCellFactory();
+        }
+
+        public void initMessageCellFactory() {
+            setCellFactory(new Callback<ListView<String>, ListCell<String>>() {
+                @Override
+                public ListCell<String> call(ListView<String> param) {
+                    return new MessageCell();
+                }
+            });
+        }
+    }
+
+    public class MessageCell extends ListCell<String> {
+        private Label texte = new Label("Contenu");
+        private ImageView avatarMessage = new ImageView(new Image("https://i1.social.s-msft.com/profile/u/avatar.jpg?displayname=kabir+shenvi&size=extralarge&version=00000000-0000-0000-0000-000000000000", 45, 45, true, false));
+
+        public MessageCell() {
+            setTexte(null);
+            initCell();
+        }
+
+        public MessageCell(String text) {
+            setTexte(text);
+            initCell();
+        }
+
+        public void setTexte(String text) {
+            if (text != null) {
+                text = text.trim().toLowerCase();
+            }
+            setText(null);
+            texte.setText(text);
+        }
+
+        public void initCell() {
+            GridPane gridPane = new GridPane();
+            gridPane.setHgap(10);
+            gridPane.setVgap(10);
+            gridPane.setPadding(new Insets(15, 15, 15, 15));
+            gridPane.add(avatarMessage, 0, 0);
+            gridPane.add(texte, 2, 0);
+            setGraphic(gridPane);
+        }
+
+        @Override
+        public void updateItem(String text, boolean empty) {
+            super.updateItem(text, empty);
+            if (empty) {
+                // setTextField(null);
+                setGraphic(null);
+            } else {
+                setTexte(text);
+                initCell();
+            }
+        }
+
+        @Override
+        public String toString() {
+            return texte.getText();
+        }
+    }
 
 
     @Override
